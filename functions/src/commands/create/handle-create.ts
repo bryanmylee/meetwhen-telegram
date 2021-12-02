ee435ac6974d5a943582d34a4cf762df2ae532b4
Bryan Lee <bryanmylee@gmail.com>
2021-12-03 00:07:50 +0800

    feat: more prompts

--- functions/src/commands/create/handle-create.ts
+++ functions/src/commands/create/handle-create.ts
@@ -1,9 +1,23 @@
 import type { Message } from 'telegram-typings';
-import type { CreateSession } from '../../types/CreateSession';
+import { CreateSession, CREATE_PROMPTS } from '../../types/CreateSession';
+import { reply } from '../../utils/reply';
+import { updateSessionWithId } from '../../db/sessions';
 
 export const handleCreate = async (session: CreateSession, message: Message): Promise<void> => {
   switch (session.latestPrompt) {
     case 'MEETING_NAME':
-      console.log('adding new meeting name!');
+      return handleMeetingName(session, message);
   }
+};
+
+const handleMeetingName = async (session: CreateSession, message: Message): Promise<void> => {
+  const newSession: CreateSession = {
+    ...session,
+    meetingName: message.text,
+    latestPrompt: 'MEETING_DATE_START',
+  };
+  updateSessionWithId(message.chat.id.toString(), newSession);
+  reply(message, {
+    text: CREATE_PROMPTS.MEETING_DATE_START,
+  });
 };
--- functions/src/types/CreateSession.ts
+++ functions/src/types/CreateSession.ts
@@ -2,6 +2,11 @@ import { Session } from './Session';
 
 export const CREATE_PROMPTS = {
   MEETING_NAME: 'Send me the name of your meet\\.',
+  MEETING_DATE_START: 'What day should we start?',
+  MEETING_DATE_END: 'What day should we end?',
+  MEETING_TIME_START: 'What time should we start?',
+  MEETING_TIME_END: 'What time should we end?',
+  CONFIRM_OR_ADVANCED: 'Confirm your meet or set more advanced options\\.',
 };
 
 export type CreatePrompt = keyof typeof CREATE_PROMPTS;
