4f18b650b60cb65e483f59f3724d02bb1a0603c0
Bryan Lee <bryanmylee@gmail.com>
2021-12-05 04:03:20 +0800

    style: i instead of we

--- functions/src/timezone/tzPrompts.ts
+++ functions/src/timezone/tzPrompts.ts
@@ -2,6 +2,6 @@ export const TZ_PROMPTS = {
   SET_TZ_LOCATION: `
 Your location will be used to determine your timezone\\.
 
-*We do not store your location*\\.`,
+*I do not store your location*\\.`,
   SET_TZ_MANUAL: 'Pick your timezone\\.',
 };
