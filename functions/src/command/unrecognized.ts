bdf8271b6706a8566cebba73db66803b84ebddaf
Bryan Lee <bryanmylee@gmail.com>
2021-12-05 03:31:20 +0800

    chore: remove dead code

--- functions/src/command/unrecognized.ts
+++
@@ -1,8 +0,0 @@
-import type { Message } from 'telegram-typings';
-import { replyToMessage } from '../utils/replyToMessage';
-
-export const unrecognized = async (message: Message): Promise<void> => {
-  await replyToMessage(message, {
-    text: 'Unrecognized command. Try again?',
-  });
-};
