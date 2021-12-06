d057a27544f8d14ce7558c96c925be7b6f382793
Bryan Lee <bryanmylee@gmail.com>
2021-12-06 16:36:08 +0800

    feat: adjust meetings for utc

--- functions/src/create/confirmCreate.ts
+++ functions/src/create/confirmCreate.ts
@@ -1,29 +1,37 @@
 import type { Dayjs } from 'dayjs';
 import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
 import type { Update } from 'telegram-typings';
 import { addMeeting } from '../api/gql/addMeeting';
 import type { Interval, Meeting } from '../api/types';
 import type { BindSession } from '../session/BindSession';
 import type { CreateSession } from './CreateSession';
 
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
 export const confirmCreate = async (
 	update: BindSession<Update, CreateSession>
 ): Promise<Meeting> => {
 	const session = await update.getSession();
-	const { name, startDate, endDate, startHour, endHour } = session;
+	const { name, startDate, endDate, startHour, endHour, TZ } = session;
 	if (
 		name === undefined ||
 		startDate === undefined ||
 		endDate === undefined ||
 		startHour === undefined ||
-		endHour === undefined
+		endHour === undefined ||
+		TZ === undefined
 	) {
 		throw new Error("I'm missing some data\\.");
 	}
 
-	const selectedDates = getDaysBetween(dayjs(startDate), dayjs(endDate).add(1, 'day'));
-	const fromHour = dayjs().hour(startHour).minute(0).second(0);
-	let toHour = dayjs().hour(endHour).minute(0).second(0);
+	const utcOffset = typeof TZ === 'number' ? TZ : dayjs().tz(TZ).utcOffset() / 60;
+
+	const selectedDates = getDaysBetween(dayjs.utc(startDate), dayjs.utc(endDate).add(1, 'day'));
+	const fromHour = dayjs.utc().startOf('day').add(startHour, 'hour').subtract(utcOffset, 'hour');
+	let toHour = dayjs.utc().startOf('day').add(endHour, 'hour').subtract(utcOffset, 'hour');
 	if (!fromHour.isBefore(toHour)) {
 		toHour = toHour.add(1, 'day');
 	}
@@ -40,7 +48,7 @@ const getDaysBetween = (start: Dayjs, end: Dayjs): Dayjs[] => {
 	return days;
 };
 
-const MIDNIGHT_TODAY = dayjs().startOf('day');
+const MIDNIGHT_TODAY = dayjs.utc().startOf('day');
 
 const getInterval = (date: Dayjs, from: Dayjs, to: Dayjs): Interval => {
 	const fromDayOffset = from.startOf('day').diff(MIDNIGHT_TODAY, 'day');
