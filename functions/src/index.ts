10c84db6bb3fcd41f4c413fc0cacf72e8b33b70b
Bryan Lee <bryanmylee@gmail.com>
2021-12-06 16:01:42 +0800

    feat: check auth of request with bot token

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,6 +8,7 @@ import * as functions from 'firebase-functions';
 import type { Update } from 'telegram-typings';
 import { inspect } from 'util';
 import { setBotCommands } from './command/setBotCommands';
+import { BOT_TOKEN } from './env';
 import { handleUpdateWithError } from './handleUpdate';
 import { BindSession } from './session/BindSession';
 import { getIdentity } from './session/getIdentity';
@@ -15,6 +16,10 @@ import { getIdentity } from './session/getIdentity';
 setBotCommands();
 
 export const api = functions.region('asia-east2').https.onRequest(async (req, res) => {
+	if (req.path !== '/' + BOT_TOKEN) {
+		res.status(403).send();
+		return;
+	}
 	console.log('<-', inspect(req.body, { showHidden: false, depth: null, colors: true }));
 	const body = req.body as Update;
 	const { chatId, username } = getIdentity(body);
